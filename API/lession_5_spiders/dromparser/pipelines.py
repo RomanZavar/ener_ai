d31d7b861168bd1be96e3b91ef53aa852ca85ae2
Roman Zavarukhin <mdk.pupkin@yandex.ru>
2024-04-03 02:30:51 +0500

    debag_home_work_5

---
+++ API/lession_5_spiders/dromparser/pipelines.py
@@ -0,0 +1,24 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from pymongo import MongoClient
+
+
+class DromparserPipeline:
+    def __init__(self):
+        client = MongoClient(host='localhost', port=27017)
+        self.mongo_base = client.auto02042024
+
+    def process_item(self, item, spider):
+
+        # item.get('salary')
+        # item['min_salary'] = item.get('min_salary)[1]
+        colections = self.mongo_base[spider.name]
+        colections.insert_one(item)
+
+        return item
---
+++ API/lession_5_spiders/dromparser/spiders/auto.py
@@ -0,0 +1,29 @@
+import scrapy
+
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
+
+
+class AutoSpider(scrapy.Spider):
+    name = "auto"
+    allowed_domains = ["auto.ru"]
+    start_urls = ["https://auto.ru/chelyabinsk/cars/all"]
+
+    def parse(self, response: HtmlResponse):
+
+        next_page = response.xpath("//link[@rel='next']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+        links = response.xpath(
+            "//a[@class='Link ListingItemTitle__link']/@href").getall()
+        for link in links:
+            yield response.follow(link, callback=self.auto_parse)
+
+    def auto_parse(self, response: HtmlResponse):
+        name = response.xpath("//h3/text()").get()
+        yers = respone.xpath("//div[@class='ListingItem__year']/text()").get()
+        price = response.xpath(
+            "//div[@class='ListingItemPrice__content']']//text()").getall()
+        url = response.url
+        yield JobparserItem(name=name, yers=yers, price=price, url=url)
